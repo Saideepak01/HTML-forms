8c80bb19a094349873a9b97a989ad0dde2bb4c2a
Sai Deepak <68103212+Saideepak01@users.noreply.github.com>
2021-09-16 13:10:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function formsubmit(){
+    let form = document.querySelector("form")
+    let formData = new FormData(form);
+    let firstName = formData.get("firstName");
+    let lastName = formData.get("lastName");
+    let inputAddress = formData.get("inputAddress");
+    // let inputAddress2 = formData.get("inputAddress2");
+    let pincode = formData.get("pincode");
+    let gender = formData.get("gender");
+    let food = formData.getAll("food");
+    let state = formData.get("state");
+    let country = formData.get("country");
+    if(food.length<2){
+        alert("choose at least 2 out of 5 choices");
+    }
+    else{
+        let tbody = document.getElementById("tbody");
+        let tr = createElement("tr");
+        let tdFirstName = createElement("td",firstName);
+        let tdLastName = createElement("td",lastName);
+        // if(inputAddress2.length !== 0)
+        //     inputAddress1 = inputAddress1 + ", " + inputAddress2;
+        let tdAddress = createElement("td",inputAddress);
+        let tdPinccode = createElement("td",pincode);
+        let tdGender = createElement("td",gender);
+        let tdFood = createElement("td", food.join(","));
+        let tdState = createElement("td",state);
+        let tdCountry = createElement("td",country);
+        tr.append(tdFirstName,tdLastName,tdAddress,tdPinccode,tdGender,tdFood,tdState,tdCountry);
+        tbody.append(tr);
+        form.reset();
+    }
+    return false;
+}
+
+function createElement(elementName,value = ""){
+    var element = document.createElement(elementName);
+    element.innerHTML = value;
+    return element;
+}
